1bec569078c8ebe8443f9c3596b6a50d0f56b7a3
lauriselvijs <lauriselvijsm@gmail.com>
2025-10-19 13:39:56 +0300

    Update docker-compose to include REVERB_PORT and refactor useNotificationBroadcast to utilize useEchoNotification

--- resources/js/hooks/useBroadcast.ts
+++ resources/js/hooks/useBroadcast.ts
@@ -1,12 +1,9 @@
 import { NOTIFICATION_CHANNEL } from "@/config/notifications";
 import { PageProps } from "@/types";
-import {
-    Notification,
-    NotificationChannel,
-    NotificationType,
-} from "@/types/Notifications";
+import { NotificationChannel, NotificationType } from "@/types/Notifications";
 import { TaskNotification } from "@/types/Task";
 import { usePage } from "@inertiajs/react";
+import { useEchoNotification } from "@laravel/echo-react";
 import { useState } from "react";
 
 export const useNotificationBroadcast = <T = TaskNotification>(
@@ -16,12 +13,14 @@ export const useNotificationBroadcast = <T = TaskNotification>(
     const { auth } = usePage<PageProps>().props;
     const [notificationId, setNotificationId] = useState("");
 
-    window.Echo.private(channel + auth.employee.id).notification(
-        (notification: Notification<T>) => {
+    useEchoNotification<T>(
+        channel + auth.employee.id,
+        (notification) => {
             if (notification.type === type) {
                 setNotificationId(notification.id);
             }
-        }
+        },
+        type
     );
 
     return notificationId;
